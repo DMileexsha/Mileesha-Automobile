e6e176515ca4b14502d038d60dc5b7cd021cf44a
Milee <dilkimileesha@gmail.com>
2025-10-10 19:31:26 +0530

    Fix case-sensitive import paths for Vercel

--- src/Components/UI/Button.jsx
+++ src/Components/UI/Button.jsx
@@ -1,11 +1,11 @@
 export function Button({ disabled, children, ...props }) {
   return (
-    <Button
+    <button
       {...props}
       className="w-full bg-red-800 hover:bg-red-600 text-lg py-3 text-white rounded disabled:opacity-80"
       disabled={disabled}
     >
       {children}
-    </Button>
+    </button>
   );
 }
